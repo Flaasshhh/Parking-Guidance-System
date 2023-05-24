5b6a25f83c1d983a97a4c381748c9538478a45d2
Flaasshhh <github@aes256.anonaddy.com>
2023-05-24 23:57:25 +0300

    Initial UI Elements Connect Customer Button with Activity

--- app/src/main/java/com/example/parkingguidancesystem/MainActivity.java
+++ app/src/main/java/com/example/parkingguidancesystem/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.parkingguidancesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+        private Button CustomerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomerButton = findViewById(R.id.CustomerButton);
+        CustomerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LaunchModule(1);
+            }
+        });
+
     }
-}
+
+
+
+    public void LaunchModule(int choice){
+        /*Intent intent1 = new Intent(this, )
+        switch (choice) {
+            case 1:
+                startActivity(intent1);
+                break;
+            // Add additional cases if needed
+            default:
+                // Handle default case if necessary
+                */
+        }
+
+    }
